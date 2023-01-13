8239ab48af00d274be7538c4d14ce22db3371365
Viktor <dodu204@mail.ru>
2023-01-13 16:46:09 +0300

    file zadanie

--- main.py
+++ main.py
@@ -1,16 +1,88 @@
-# This is a sample Python script.
+def greet():
+    print('-------------------')
+    print(' Приветствуем вас  ')
+    print('      в игре       ')
+    print(' Крестики-нолики  ')
+    print('-------------------')
+    print(' формат ввода: x y ')
+    print(' x - номер строки  ')
+    print(' y - номер столбца ')
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+def show():
+    print()
+    print('   | 0 | 1 | 2 | ')
+    print('-------------------')
+    for i, row in enumerate(field):
+        row_str = f" {i} | {' | '.join(row)} | "
+        print(row_str)
+        print('-------------------')
+    print()
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def ask():
+    while True:
+        cords = input('   Ваш ход: ').split()
+
+        if len(cords) != 2:
+            print(' Введите две координаты. ')
+            continue
+
+        x, y = cords
+
+        if not (x.isdigit()) or not (y.isdigit()):
+            print(' Введите числа! ')
+            continue
+
+        x, y = int(x), int(y)
+
+        if 0 > x or x > 2 or 0 > y or y > 2:
+            print(' Координаты вне диапазона. ')
+            continue
+
+        if field[x][y] != ' ':
+            print(' Клетка занята.')
+            continue
+
+        return x, y
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def cheсk_win():
+    win_cord = (((0, 0), (0, 1), (0, 2)), ((1, 0), (1, 1), (1, 2)), ((2, 0), (2, 1), (2, 2)),
+                ((0, 2), (1, 1), (2, 0)), ((0, 0), (1, 1), (2, 2)), ((0, 0), (1, 0), (2, 0)),
+                ((0, 1), (1, 1), (2, 1)), ((0, 2), (1, 2), (2, 2)))
+    for cord in win_cord:
+        symbols = []
+        for c in cord:
+            symbols.append(field[c[0]][c[1]])
+        if symbols == ['X', 'X', 'X']:
+            print(' Выиграл X')
+            return True
+        if symbols == ['0', '0', '0']:
+            print(' Выиграл 0')
+            return True
+    return False
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+greet()
+field = [[' '] * 3 for i in range(3)]
+count = 0
+while True:
+    count += 1
+    show()
+    if count % 2 == 1:
+        print(' Ходит крестик. ')
+    else:
+        print(' Ходит нолик. ')
+
+    x, y = ask()
+    if count % 2 == 1:
+        field[x][y] = 'X'
+    else:
+        field[x][y] = '0'
+
+    if cheсk_win():
+        break
+    if count == 9:
+        print(' Ничья. ')
+        break
